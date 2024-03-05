cd54c97e2920e953614c31274da3528895deb69b
zisek <34651196+zisek@users.noreply.github.com>
2024-03-05 21:23:42 +0100

    Create confix.py

---
+++ confix.py
@@ -0,0 +1,10 @@
+import os
+
+# Set application path to current dir
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+# Debug config, set to False for hosting
+DEBUG = True
+
+# Secret key - set something long and complex
+SECRET_KEY = 'Hoodne dlouhy super tajny sroubovak!'
